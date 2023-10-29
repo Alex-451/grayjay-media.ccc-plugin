32e27af8209ba559ab51a1d8f0babd8722638018
Alex <sepp.amsl@protonmail.com>
2023-10-29 15:38:18 +0100

    Init

---
+++ src/MediaCccScript.js
@@ -0,0 +1,36 @@
+const URL_CONTENT = "https://api.media.ccc.de/public/"
+
+const PLATFORM = "media.ccc.de";
+
+
+var config = {};
+
+//Source Methods
+source.enable = function (conf) {
+    /**
+     * @param conf: SourceV8PluginConfig (the SomeConfig.js)
+     */
+}
+
+source.getHome = function(continuationToken) {
+    const videos = getCccContentData(); // The results (PlatformVideo)
+    return new getRecentPager(videos);
+}
+
+//Internals
+function getCccContentData() {
+	const resp = http.GET(URL_CONTENT+"events/recent/", {});
+	const contentResp = JSON.parse(resp.body);
+	
+	return contentResp.events;
+}
+function getRecentPager(query) {
+	return new RecentPager(query);
+}
+
+//Pagers
+class RecentPager extends VideoPager {
+	constructor(results) {
+		super(results);
+	}
+}
